c5ea685039515c46c479902388dd67f60d222a38
Arunadevi <l.arunadevi@gmail.com>
2013-07-24 21:47:26 +0530

    Support for dragging and dropping force elements at an approximate position

--- public/javascripts/emtool.js
+++ public/javascripts/emtool.js
@@ -16,7 +16,58 @@ function drop(ev,hexag_array,stage,layer)
 {
 ev.preventDefault();
 var data=ev.dataTransfer.getData("Text");
-hexag_array[0].setFill("blue");
-layer.add(hexag_array[0]);
+
+// Get content element description
+var r = $("#container");   
+var p = $((r.children().first()).children().first());
+var off = p.offset(); 
+var e = $("#"+data);
+console.log($(e.parent().prev()).html()); // Content element description
+
+// Get co-ordinates to position id text
+var xpos = ev.clientX-off.left;
+var ypos = ev.clientY-off.top;
+var radius = hexag_array[0].getRadius();
+var col = Math.round((xpos-20)/((2*radius)+3));
+var row = Math.round((ypos)/((2*radius)+3));
+alert(col);
+alert(row);
+var textX = ((col*((2*radius)+3))+radius);
+var textY = ((row*((2*radius)+3)));
+alert(textX);
+alert(textY);
+
+// Create text element
+var simpleText = new Kinetic.Text({
+    x: textX,
+    y: textY,
+    text: data,
+    fontSize: 10,
+    fontFamily: 'Calibri',
+  });
+
+
+/*
+// Create group
+var group = new Kinetic.Group({
+    
+});
+group.add(simpleText);
+group.add(hexag_array[(col*80)+row]);
+layer.add(group);*/
+
 stage.add(layer);
+console.log(stage.getAbsolutePosition());
+console.log(off.left);
+console.log(ev.clientX +" : "+ ev.clientY);
+/*var mousexy = stage.getMousePosition();
+var mx = mousexy.x;*/
+//console.log("11111111"+mx);
+ // console.log(stage.getX()); //0
+//console.log(stage.get('#he')[0]); not working
+//console.log(stage.getMousePosition()); not working
+//console.log($(ev.target).position().left); = 0
+//console.log($(ev.target).position().x); 
+//console.log(ev.layerX); = 0
+
 }
