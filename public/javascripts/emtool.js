d08299e7d21d828f6d90239fe870679feb8e083f
Arunadevi <l.arunadevi@gmail.com>
2013-07-25 11:59:43 +0530

    Support for dragging and dropping force elements

--- public/javascripts/emtool.js
+++ public/javascripts/emtool.js
@@ -8,14 +8,18 @@ ev.preventDefault();
 
 function drag(ev)
 {
-// add the div back to the parent of the div
+
 ev.dataTransfer.setData("Text",ev.target.id);
 }
 
-function drop(ev,hexag_array,stage,layer)
+function drop(ev,hexag_array,stage,layer,gridHeight)
 {
 ev.preventDefault();
 var data=ev.dataTransfer.getData("Text");
+
+var radius = hexag_array[0].getRadius();
+var height = (radius * Math.sqrt(3));
+var side = radius * 3 / 2;
 
 // Get content element description
 var r = $("#container");   
@@ -24,25 +28,28 @@ var off = p.offset();
 var e = $("#"+data);
 console.log($(e.parent().prev()).html());
 
-// Get co-ordinates to position id text
-var xpos = ev.clientX-off.left;
-var ypos = ev.clientY-off.top;
-var radius = hexag_array[0].getRadius();
-var col = Math.round((xpos-20)/((2*radius)+3));
-var row = Math.round((ypos)/((2*radius)+3));
-alert(col);
-alert(row);
-var textX = (col*((2*radius)+3));
-var textY = (row*((2*radius)+3));
-alert(textX);
-alert(textY);
+// Get pixel co-ordinates relative to canvas/stage to position id text
+var xpos= ev.pageX-off.left;
+var ypos = ev.pageY-off.top;
+
+// Find matrix co-ordinates
+var col = Math.round((xpos-radius)/(side+3));
+var i1 = col%2;
+var i2 = (i1*height)/2;
+var i3 = ypos - radius - i2;
+var row = Math.round(i3 / (height+4));
+
+// Get pixel co-ordinates of hexagon on the canvas
+var arr_index = (col*gridHeight)+row;
+var textX = ((hexag_array[arr_index]).getX()) - Math.round(radius/2);
+var textY = ((hexag_array[arr_index]).getY());
 var simpleText = new Kinetic.Text({
     x: textX,
     y: textY,
     text: data,
     fontSize: 10,
     fontFamily: 'Calibri',
-    fill: 'green'
+    fill: 'white'
   });
 
 /*
@@ -52,19 +59,9 @@ var group = new Kinetic.Group({
 group.add(hexag_array[(col*80)+row]);
 group.add(simpleText);
 layer.add(group);*/
-
+hexag_array[arr_index].setFill("blue");
+layer.add(hexag_array[arr_index]);
 layer.add(simpleText);
 stage.add(layer);
-console.log(stage.getAbsolutePosition());
-console.log(off.left);
-console.log(ev.clientX +" : "+ ev.clientY)
-/*var mousexy = stage.getMousePosition();
-var mx = mousexy.x;*/
-//console.log("11111111"+mx);
-      // console.log(stage.getX()); //0
-//console.log(stage.get('#he')[0]); not working
-//console.log(stage.getMousePosition()); not working
-//console.log($(ev.target).position().left); = 0
-//console.log($(ev.target).position().x); 
-//console.log(ev.layerX); = 0   
+
 }
