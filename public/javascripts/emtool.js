dcc5ec8e7b8269174a9641cc6b23c22cb947ebb9
Arunadevi <l.arunadevi@gmail.com>
2013-07-25 16:06:38 +0530

    Add support for dragging and dropping stories and components and a tooltip

--- public/javascripts/emtool.js
+++ public/javascripts/emtool.js
@@ -26,6 +26,7 @@ var r = $("#container");
 var p = $((r.children().first()).children().first());
 var off = p.offset(); 
 var e = $("#"+data);
+var desc = $(e.parent().prev()).html();
 console.log($(e.parent().prev()).html());
 
 // Get pixel co-ordinates relative to canvas/stage to position id text
@@ -38,6 +39,16 @@ var i1 = col%2;
 var i2 = (i1*height)/2;
 var i3 = ypos - radius - i2;
 var row = Math.round(i3 / (height+4));
+
+// Color coding based on type of content element
+var hexcolor = 'white';
+if(data.indexOf('F') == 0) {
+	hexcolor = 'blue';
+} else if(data.indexOf('S')===0) {
+	hexcolor ='red';
+} else {
+	hexcolor='yellow';
+}
 
 // Get pixel co-ordinates of hexagon on the canvas
 var arr_index = (col*gridHeight)+row;
@@ -52,6 +63,46 @@ var simpleText = new Kinetic.Text({
     fill: 'white'
   });
 
+/*  Adding Tool tip */
+var tooltip = new Kinetic.Label({
+        x: 170,
+        y: 75,
+        opacity: 0.75
+      });
+
+      tooltip.add(new Kinetic.Tag({
+        fill: 'black',
+        pointerDirection: 'down',
+        pointerWidth: 10,
+        pointerHeight: 10,
+        lineJoin: 'round',
+        shadowColor: 'black',
+        shadowBlur: 10,
+        shadowOffset: 10,
+        shadowOpacity: 0.5
+      }));
+      
+   tooltip.add(new Kinetic.Text({
+        text: desc,
+        fontFamily: 'Calibri',
+        fontSize: 14,
+        padding: 1,
+        fill: 'white'
+      }));
+
+hexag_array[arr_index].on("mousemove", function(){
+                
+                tooltip.setPosition(textX + 5, textY + 5);               
+                tooltip.show();
+                layer.draw();
+                stage.add(layer);
+            });
+ 
+hexag_array[arr_index].on("mouseout", function(){
+                tooltip.hide();
+                layer.draw();
+                stage.add(layer);
+            });
 /*
 // Creating group
 var group = new Kinetic.Group({ 
@@ -59,9 +110,10 @@ var group = new Kinetic.Group({
 group.add(hexag_array[(col*80)+row]);
 group.add(simpleText);
 layer.add(group);*/
-hexag_array[arr_index].setFill("blue");
+hexag_array[arr_index].setFill(hexcolor);
 layer.add(hexag_array[arr_index]);
 layer.add(simpleText);
+layer.add(tooltip);
 stage.add(layer);
 
 }
