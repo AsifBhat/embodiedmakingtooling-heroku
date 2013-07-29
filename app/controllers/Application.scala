9a0174deead0179b59f527b99d70ede98fd94371
Arunadevi <l.arunadevi@gmail.com>
2013-07-29 16:54:15 +0530

    Merge changes

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,10 +8,6 @@ import views._
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("The Embodied Making Tool",Story.all(),Force.all(),ForceInteraction.all(),SolutionComponent.all()))
-  }
-
   def worksheet = Action {
     Ok(views.html.worksheet("The Embodied Making Tool",Story.all(),Force.all(),ForceInteraction.all(),SolutionComponent.all()))
   }
