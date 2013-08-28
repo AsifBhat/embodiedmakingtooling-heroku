ca871c9dd6c1ac7f5bb8d77618f9041be814387d
Arunadevi <l.arunadevi@gmail.com>
2013-08-28 17:08:38 +0530

    Add dependency for the graph library

--- project/Build.scala
+++ project/Build.scala
@@ -10,6 +10,7 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
       "org.scalatest" %% "scalatest" % "1.9.1" % "test",
+     "com.assembla.scala-incubator" % "graph-core_2.10" % "1.6.2",
     jdbc,
     anorm
   )
