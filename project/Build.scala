7d1f6b5a67edc6cef5a8a25ffcc33e3863c98231
piu-sharma <dante4002@gmail.com>
2013-09-02 11:37:34 +0530

    Updating build file for pointing play framework to scala 2.10.1 - Compilation issue resolved

--- project/Build.scala
+++ project/Build.scala
@@ -6,18 +6,21 @@ object ApplicationBuild extends Build {
 
   val appName         = "EMtool"
   val appVersion      = "1.0-SNAPSHOT"
+    
 
   val appDependencies = Seq(
     // Add your project dependencies here,
       "org.scalatest" %% "scalatest" % "1.9.1" % "test",
-     "com.assembla.scala-incubator" % "graph-core_2.10" % "1.6.2",
+     "com.assembla.scala-incubator" % "graph-core_2.10" % "1.6.2" exclude("org.scala-lang", "scala-actors"),
     jdbc,
     anorm
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here    
+    // Add your own project settings here
+      scalaVersion := "2.10.1",
+      autoScalaLibrary := false,
       testOptions in Test := Nil
   )
 
