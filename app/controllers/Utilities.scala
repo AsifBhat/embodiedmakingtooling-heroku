caa45a6c375998efa5592124558d60f530b70710
Arunadevi <l.arunadevi@gmail.com>
2013-10-03 14:42:38 +0530

    Write cluster to gexf file - server side

---
+++ app/controllers/Utilities.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import play.api.mvc._
+import services.ExportGexf
+
+object Utilities extends Controller {
+  
+  def exportAsGexf(fileName: String) = Action {
+    ExportGexf.export(fileName)
+    Ok("Exported")
+  }  
+}
