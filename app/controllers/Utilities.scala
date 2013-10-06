8c3e69953b6ed60bcd675e1167b918f437f41e7c
Andre Kampert <andre@cadimira.com>
2013-10-06 21:53:12 +0200

    Do not use intermediary file for download

--- app/controllers/Utilities.scala
+++ app/controllers/Utilities.scala
@@ -4,9 +4,8 @@ import play.api.mvc._
 import services.ExportGexf
 
 object Utilities extends Controller {
-  
-  def exportAsGexf(fileName: String) = Action {
-    ExportGexf.export("public/exports/clusters.gexf")
-    Ok("Exported")
-  }  
+
+  def exportAsGexf = Action {
+    Ok(ExportGexf.export).withHeaders("Content-Disposition" -> "attachment; filename=clusters.gexf")
+  }
 }
