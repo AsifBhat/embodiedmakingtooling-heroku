216c653f79508f2d540a1d659f615138c4fb519a
Arunadevi <aruna@coena.com>
2013-11-25 12:32:31 +0530

    Change LESS to CSS

--- project/Build.scala
+++ project/Build.scala
@@ -14,8 +14,6 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
       "org.scalatest" %% "scalatest" % "1.9.1" % "test",
      "com.assembla.scala-incubator" % "graph-core_2.10" % "1.6.2" exclude("org.scala-lang", "scala-actors"),
-     /*"org.scala.sbt" % "scripted-plugin" % "" % "0.12.2",
-     "me.lessis" % "bintray-sbt" % "2.10" % "0.13" % "0.1.1",*/
     jdbc,
     anorm
   )
@@ -25,7 +23,6 @@ object ApplicationBuild extends Build {
   val jasmimePluginProject = Project("sbtJasminePlugin", file("sbt-jasmine-plugin"))
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here
       scalaVersion := "2.10.1",
       autoScalaLibrary := false,
       testOptions in Test := Nil,
@@ -55,33 +52,29 @@ object ApplicationBuild extends Build {
   .settings(
 	  sourceDirectory in (Compile, CoffeeKeys.coffee) <<= (sourceDirectory in Compile)(_ / "assets" / "javascripts"),
 	  resourceManaged in (Compile, CoffeeKeys.coffee) <<= (resourceManaged in Compile)(_ / "public" / "javascripts")
-   )
+  )
 
-
- /* .settings(lessSettings : _*)
+  /* .settings(lessSettings : _*)
   .settings(
     (sourceDirectory in (Compile, LessKeys.less)) <<= (sourceDirectory in Compile)(_ / "assets" / "stylesheets" ),
     //(resourceManaged in (Compile, StyleKeys.less)) <<= (sourceDirectory in Compile)(_ / "public" / "stylesheets")
     (resourceManaged in (Compile, LessKeys.less)) <<= (crossTarget in Compile)(_ / "public" / "stylesheets")
   )*/
 
-
   .settings(jasmineSettings : _*)  //this adds jasmine settings from the sbt-jasmine plugin into the project
   .settings(
     // jasmine configuration, overridden as we don't follow the default project structure sbt-jasmine expects
-  appJsDir <+= baseDirectory / "app/assets/javascripts",
-  appJsLibDir <+= baseDirectory / "public/javascripts",    
-  jasmineTestDir <+= baseDirectory / "test/assets/",
-  jasmineConfFile <+= baseDirectory / "test/assets/test.dependencies.js",
-  // link jasmine to the standard 'sbt test' action. Now when running 'test' jasmine tests will be run, and if they pass
-  // then other Play tests will be executed.
-  //(compile in Compile) <<= (compile in Compile) dependsOn (LessKeys.less in Compile),
-  (test in Test) <<= (test in Test) dependsOn (jasmine),
-  (jasmine) <<= (jasmine) dependsOn (CoffeeKeys.coffee in Compile),
-  (playCopyAssets) <<= (playCopyAssets) dependsOn (copyAssets)
-
-  )
- 
+    appJsDir <+= baseDirectory / "app/assets/javascripts",
+    appJsLibDir <+= baseDirectory / "public/javascripts",    
+    jasmineTestDir <+= baseDirectory / "test/assets/",
+    jasmineConfFile <+= baseDirectory / "test/assets/test.dependencies.js",
+    // link jasmine to the standard 'sbt test' action. Now when running 'test' jasmine tests will be run, and if they pass
+    // then other Play tests will be executed.
+    //(compile in Compile) <<= (compile in Compile) dependsOn (LessKeys.less in Compile),
+    (test in Test) <<= (test in Test) dependsOn (jasmine),
+    (jasmine) <<= (jasmine) dependsOn (CoffeeKeys.coffee in Compile),
+    (playCopyAssets) <<= (playCopyAssets) dependsOn (copyAssets)
+  ) 
   .dependsOn(jasmimePluginProject).aggregate(jasmimePluginProject)
 
 
